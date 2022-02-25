5729e5b22e773b476ff3fd8b101adb5b9e9c199a
ginlink <ginlink@163.com>
2022-02-25 15:36:31 +0800

    feat: add edit

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Typography } from '@mui/material'
+import { Box, Card, Container, Stack, Typography } from '@mui/material'
 import { NextPage } from 'next'
 import { useEffect } from 'react'
 import Page from '../src/components/Page'
@@ -8,9 +8,14 @@ const Edit: NextPage = () => {
 
   return (
     <Page title="Edit | host">
-      <Box>
-        <Typography>123</Typography>
-      </Box>
+      <Container>
+        <Box>
+          <Stack direction={'row'} spacing={2}>
+            <Typography>123</Typography>
+            <Typography>123</Typography>
+          </Stack>
+        </Box>
+      </Container>
     </Page>
   )
 }
